4fe8fa5b9e6f2765c0e9b60e13ceb0e775b4e6e2
valerieshoker <102839492+valerieshoker@users.noreply.github.com>
2025-07-09 12:07:12 -0700

    this update is mostly bc im hungry

--- src/components/Hotspot.jsx
+++ src/components/Hotspot.jsx
@@ -7,6 +7,9 @@ import Projects from "./Projects";
 import About from "./About";
 import bookshelfImg from "../assets/Bookshelf.png";
 import armchairImg from '../assets/armchair.png';
+import sidetableImg from '../assets/sidetable.png';
+import croissantfullImg from '../assets/croissantfull.png';
+import espressoImg from '../assets/espresso.png';
 
 export default function Hotspot() {
   return (
@@ -21,6 +24,22 @@ export default function Hotspot() {
         alt="Armchair"
         className="absolute bottom-[5px] left-[65%] w-[360px] object-contain"
       />
+        <img
+        src={sidetableImg}
+        alt="SideTable"
+        className="absolute bottom-[30px] left-[46%] w-[280px] object-contain"
+      />
+        <img
+        src={croissantfullImg}
+        alt="CroissantFull"
+        className="absolute bottom-[213px] left-[59%] w-[80px] object-contain"
+      />
+       <img
+        src={espressoImg}
+        alt="Espresso"
+        className="absolute bottom-[213px] left-[53.5%] w-[65px] object-contain"
+      />
+
 
       {/* Modular objects with animation + navigation */}
       <Projects />
